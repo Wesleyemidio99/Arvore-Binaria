f8a28c8e0d09cc31f20fbc1588061b3f4ed0d41e
wesley <wesleyemidio99@hotmail.com>
2023-05-20 12:28:55 -0300

    Atualização de Arvore Binaria de forma Recursiva

--- main.c
+++ main.c
@@ -26,15 +26,34 @@ Arvore * criar(char info,Arvore * SAE, Arvore * SAD){
 
 }
 
+void inserir(Arvore **no,char letra){
+    if(*no == NULL){
+        *no =(Arvore *) malloc(sizeof(Arvore));
+        *no->esquerda = inicializarArvore();
+        *no -> direitra = inicializarArvore();
+        *no -> info = letra;
+    }
+    else{
+        if(letra < (*no) -> esquerda){
+            inserir(&(*no) ->esquerda,letra);
+        }
+        else{
+            inserir(&(*no) ->direita, letra);
+        }
+    }
+}
+
 int main() {
     Arvore * A = inicializarArvore();
 
-    Arvore * D = criar('d',inicializarArvore(),inicializarArvore());
+    inserir(&A,'c');
+    /*Arvore * D = criar('d',inicializarArvore(),inicializarArvore());
     Arvore * E = criar('e',inicializarArvore(),inicializarArvore());
     Arvore * F = criar('f',inicializarArvore(),inicializarArvore());
     Arvore * B = criar('b',inicializarArvore(),D);
     Arvore * C = criar('c',E,F);
-    A = criar('a',B,C);
+    A = criar('a',B,C);*/
+
 
     return 0;
 }
